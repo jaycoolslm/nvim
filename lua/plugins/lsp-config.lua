f87ff318c1e2e4902ccf8035a66448935123bf07
Jake Hall <jake.hallslm@gmail.com>
2024-08-01 08:02:48 +0100

    copilot

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,57 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "svelte", "jsonls" },
-				-- auto_install = true
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			--local capabilities = vim.tbl_deep_extend(
-			--"force",
-			--vim.lsp.protocol.make_client_capabilities(),
-			--require("cmp_nvim_lsp").default_capabilities()
-			--)
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "svelte", "jsonls", "bashls" },
+        -- auto_install = true
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      --local capabilities = vim.tbl_deep_extend(
+      --"force",
+      --vim.lsp.protocol.make_client_capabilities(),
+      --require("cmp_nvim_lsp").default_capabilities()
+      --)
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-
-      lspconfig.svelte.setup({
-        capabilities = capabilities
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
       })
 
-			lspconfig.jedi_language_server.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.svelte.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, {})
-		end,
-	},
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gl", vim.diagnostic.open_float, {})
+    end,
+  },
 }
