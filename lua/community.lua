028b60499a249c3672267533b25c54e0daa3b4fd
Jake Hall <jaycoolslm@gmail.com>
2026-08-17 13:36:42 +0800

    clean up plugins

--- lua/community.lua
+++ lua/community.lua
@@ -13,11 +13,10 @@ return {
   { import = "astrocommunity.pack.mdx" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.sql" },
-  { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.xml" },
   { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
   -- import/override with your plugins folder
 }
